c96f6d28598e996bcc0d540bf0b46707c58315e9
Your Name <taboubimahdi1920@gmail.com>
2022-01-02 21:29:14 +0100

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,31 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {
+      
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPass,
+      });
+  
+      const user = await newUser.save();
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+  module.exports = router
